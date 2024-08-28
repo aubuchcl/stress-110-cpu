231056174beff10719bbd83df547d0073d545315
Christopher Aubuchon <aubuchcl@gmail.com>
2024-08-28 13:54:21 -0700

    updated script

--- script.sh
+++ script.sh
@@ -7,8 +7,19 @@ CPU_COUNT=${CPU_COUNT:-1}
 # 110% is simulated by overcommitting the number of workers (e.g., n * 1.1)
 WORKER_COUNT=$(echo "$CPU_COUNT * 1.1" | bc | awk '{print int($1+0.5)}')
 
-# Execute stress-ng
-stress-ng --cpu "$CPU_COUNT" --cpu-load 100 --cpu-method all --timeout 60s &
-stress-ng --cpu $(($WORKER_COUNT - $CPU_COUNT)) --cpu-load 10 --cpu-method all --timeout 60s
+# Function to handle SIGINT
+handle_sigint() {
+    echo "SIGINT received, stopping stress test..."
+    pkill -SIGINT stress-ng
+    exit 0
+}
 
+# Trap SIGINT signal and call handle_sigint
+trap 'handle_sigint' SIGINT
+
+# Execute stress-ng for 10 minutes
+stress-ng --cpu "$CPU_COUNT" --cpu-load 100 --cpu-method all --timeout 600s &
+stress-ng --cpu $(($WORKER_COUNT - $CPU_COUNT)) --cpu-load 10 --cpu-method all --timeout 600s
+
+# Wait for all background processes to finish
 wait
